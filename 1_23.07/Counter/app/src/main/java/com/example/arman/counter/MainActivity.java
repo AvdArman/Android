087376184822c8681b37503569db609d0acfbb72
Arman <avdalyan1999@gmail.com>
2018-07-24 02:36:58 +0400

    Added fifth task(counter)

---
+++ 1_23.07/Counter/app/src/main/java/com/example/arman/counter/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.arman.counter;
+
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String KEY_INDEX = "key1";
+    private TextView tvCounter;
+    private Button btnInc;
+    private Button btnDec;
+    private Integer count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Configuration.ORIENTATION_PORTRAIT == MainActivity.this.getResources().getConfiguration().orientation) {
+            setContentView(R.layout.activity_main);
+        } else {
+            setContentView(R.layout.activity_main_land);
+        }
+        tvCounter = findViewById(R.id.tv_count);
+        btnInc = findViewById(R.id.btn_inc);
+        btnDec = findViewById(R.id.btn_dec);
+        btnInc.setOnClickListener(this);
+        btnDec.setOnClickListener(this);
+        if (savedInstanceState != null) {
+            count = Integer.parseInt(savedInstanceState.getString(KEY_INDEX));
+        }
+        tvCounter.setText(count.toString());
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_inc:
+                ++count;
+                break;
+            case R.id.btn_dec:
+                --count;
+                break;
+        }
+
+        tvCounter.setText(count.toString());
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(KEY_INDEX, tvCounter.getText().toString());
+    }
+}
